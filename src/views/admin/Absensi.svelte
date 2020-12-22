7c25e3a88a5fe3df10a6a616857005f57047bc39
Rizqi Pratama <rev4rmy@gmail.com>
2020-12-22 15:41:21 +0700

    Intial 2 Commit

---
+++ src/views/admin/Absensi.svelte
@@ -0,0 +1,17 @@
+<script>
+  // core components
+  import CardPageVisits from "components/Cards/CardPageVisits.svelte";
+  import CardSocialTraffic from "components/Cards/CardSocialTraffic.svelte";
+  export let location;
+</script>
+
+<div>
+  <div class="flex flex-wrap mt-4">
+    <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+      <CardPageVisits />
+    </div>
+    <div class="w-full xl:w-4/12 px-4">
+      <CardSocialTraffic />
+    </div>
+  </div>
+</div>
